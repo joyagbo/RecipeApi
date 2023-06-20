fdd7f80a2cdeb9f3db216a699e738de6a19ecaef
joy <joyagbo49@gmail.com>
2023-06-20 01:08:28 +0100

    jwt verification

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const verifyToken = (req, res, next) => {
+    const bearHeader = req.headers["authorization"]
+    if(typeof bearHeader == "undefined") {
+        res.status(403).json([{ message: "token required" }])
+    }
+    else {
+        try {
+            const bearer = bearHeader.split(' ')
+            const b_token = bearer[1]
+            req.token = b_token
+            req.decoded = jwt.verify(b_token, "secret")
+
+            next();
+        }
+        catch (err) {
+            console.log("invalid Token")
+            res.status(403).json([{ message: "invalid Token" }])
+        }
+    }
+  };
+  
+
+  module.exports = {verifyToken}
+
+
+  
