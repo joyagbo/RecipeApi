b157837514d765309f757390085f4591263a58eb
joy <joyagbo49@gmail.com>
2023-06-20 01:07:04 +0100

    added new routes

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,19 @@
 const express = require('express')
-const rtm = express.Router()
+const {userLogin, registerUser,getUsers,
+    updateUser,
+    deleteUser,} = require('../controllers/user.controller');
+const { getRecipes, createRecipe } = require('../controllers/recipe.controller');
+const { verifyToken } = require('../middleware/auth');
+const route = express.Router()
+
+route.post('/login', verifyToken,userLogin);
+route.post('/register',registerUser)
+route.get('/users', getUsers);
+route.put('/users/:id', updateUser);
+route.delete('/users/:id', deleteUser);
+route.post('/recipe', createRecipe)
+route.get('/recipe', getRecipes)
 
 
 
-module.exports = {rtm}
+module.exports = {route}
