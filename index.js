ccfc444e37c0d95e4a952e96f12795b1a8a5a182
joy <joyagbo49@gmail.com>
2023-06-20 01:11:07 +0100

    renamed router

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const sequelize = require('./config/connect.js')
-const { rtm } = require('./routes/route.js')
+const { route } = require('./routes/route.js')
 const app = express()
 const port = 3000
 dotenv.config()
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/',rtm)
+app.use('/',route)
 
 
 app.listen(port, () => {
